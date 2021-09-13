5cefd0ef2a5e93958fe66a15d1a057196be70544
Henrik Ilgen <henrik.ilgen@aclue.de>
2021-09-13 16:23:53 +0200

    Unit tests for where and select

---
+++ test/intermediate/select.lua
@@ -0,0 +1,61 @@
+describe("intermediate function 'select'", function()
+	local linq = require("lazylualinq")
+
+	it("transforms items using the projection", function()
+		local iterator = linq { 1, 2, 3, 4 }
+			:select(function(v, k) return v % 3, k end)
+			:getIterator()
+
+		assert.is_same({iterator()}, { 1, 1 })
+		assert.is_same({iterator()}, { 2, 2 })
+		assert.is_same({iterator()}, { 0, 3 })
+		assert.is_same({iterator()}, { 1, 4 })
+
+		assert.is_same({iterator()}, { nil, nil })
+	end)
+
+	it("passes the source key to the projection", function()
+		local source = linq {
+			hello = 123
+		}
+
+		local iterator = source:select(function(_, k) return #k, k end)
+			:getIterator()
+
+		assert.is_same({iterator()}, { 5, "hello" })
+
+		assert.is_same({iterator()}, { nil, nil })
+	end)
+
+	it("accepts string lambdas", function() 
+		local iterator = linq { 1, 2, 3, 4 }
+			:select("v % 3, k")
+			:getIterator()
+
+		assert.is_same({iterator()}, { 1, 1 })
+		assert.is_same({iterator()}, { 2, 2 })
+		assert.is_same({iterator()}, { 0, 3 })
+		assert.is_same({iterator()}, { 1, 4 })
+
+		assert.is_same({iterator()}, { nil, nil })
+	end)
+
+	it("streams the source lazily", function() 
+		local iteratorCalls = 0
+
+		local iterator = linq.iterator(function() 
+				iteratorCalls = iteratorCalls + 1
+				return iteratorCalls, iteratorCalls
+			end)
+			:select(function(v, k) return v, k end)
+			:getIterator()
+
+		assert.is_same(iteratorCalls, 0)
+
+		assert.is_same({iterator()}, { 1, 1 })
+		assert.is_same(iteratorCalls, 1)
+
+		assert.is_same({iterator()}, { 2, 2 })
+		assert.is_same(iteratorCalls, 2)
+	end)
+end)
---
+++ test/intermediate/where.lua
@@ -0,0 +1,58 @@
+describe("intermediate function 'where'", function()
+	local linq = require("lazylualinq")
+
+	it("filters items that don't match the predicate", function()
+		local iterator = linq { 1, 2, 3, 4 }
+			:where(function(v) return v % 2 == 0 end)
+			:getIterator()
+
+		assert.is_same({iterator()}, { 2, 2 })
+		assert.is_same({iterator()}, { 4, 4 })
+
+		assert.is_same({iterator()}, { nil, nil })
+	end)
+
+	it("passes the source key to the predicate", function()
+		local source = linq {
+			hello = 123,
+			abc = 456
+		}
+
+		local iterator = source:where(function(_, k) return #k == 3 end)
+			:getIterator()
+
+		assert.is_same({iterator()}, { 456, "abc" })
+
+		assert.is_same({iterator()}, { nil, nil })
+	end)
+
+	it("accepts string lambdas", function() 
+		local iterator = linq { 1, 2, 3, 4 }
+			:where("v % 2 == 0")
+			:getIterator()
+
+		assert.is_same({iterator()}, { 2, 2 })
+		assert.is_same({iterator()}, { 4, 4 })
+
+		assert.is_same({iterator()}, { nil, nil })
+	end)
+
+	it("streams the source lazily", function() 
+		local iteratorCalls = 0
+
+		local iterator = linq.iterator(function() 
+				iteratorCalls = iteratorCalls + 1
+				return iteratorCalls, iteratorCalls
+			end)
+			:where(function() return true end)
+			:getIterator()
+
+		assert.is_same(iteratorCalls, 0)
+
+		assert.is_same({iterator()}, { 1, 1 })
+		assert.is_same(iteratorCalls, 1)
+
+		assert.is_same({iterator()}, { 2, 2 })
+		assert.is_same(iteratorCalls, 2)
+	end)
+end)
